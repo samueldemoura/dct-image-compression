0b17e696745b532fce316ac3dbd4004db8ff91c0
samueldemoura <samueldemouramoreira@gmail.com>
2018-06-07 10:53:33 -0300

    Initial commit

---
+++ dct.py
@@ -0,0 +1,31 @@
+import math
+import matplotlib.pyplot as plt
+
+def dct(x):
+    N = len(x)
+    X = [0]*N
+
+    for k in range(0, N):
+        if k == 0:
+            ck = math.sqrt(0.5)
+        else:
+            ck = 1
+
+        _sum = 0
+        for n in range(0, N):
+            theta = 2*math.pi * (k / (2*N)) * n + (k*math.pi) / (2*N)
+            _sum += x[n] * math.cos(theta)
+        
+        X[k] = math.sqrt(2/N) * ck * _sum
+
+    return X
+
+# Testing
+x = [0] * 26
+for i in range(0, 26):
+    x[i] =  math.cos(2*math.pi*10*i * (math.pi / 180)) #math.cos(100*i * (math.pi / 180))
+
+X = dct(x)
+plt.plot(x)
+plt.plot(X)
+plt.show()

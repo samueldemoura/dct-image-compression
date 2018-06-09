6690da498585c38158f3ed6e7bb94de71b65fa84
samueldemoura <samueldemouramoreira@gmail.com>
2018-06-09 10:14:53 -0300

    Implement 2D DCT

--- dct.py
+++ dct.py
@@ -1,7 +1,10 @@
 import math
 import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
 
 def dct(x, inverse=False):
+    '''Applies DCT transformation to input array x, returns array of coeficients.'''
     N = len(x)
     X = [0]*N
 
@@ -24,12 +27,42 @@ def dct(x, inverse=False):
 
     return X
 
-# Testing
-x = [0] * 26
-for i in range(0, 26):
-    x[i] =  math.cos(2*math.pi*10*i * (math.pi / 180)) #math.cos(100*i * (math.pi / 180))
+def dct_2d(x, w, h, inverse=False):
+    '''Applies DCT transformation repeatedly for each row and column.'''
+    X = np.array([0]*(w * h)).reshape(w, h)
 
-X = dct(x)
-plt.plot(x)
-plt.plot(X)
-plt.show()
+    for j in range(0, h):
+        X[:, j] = dct(x[:, j], inverse)
+
+    for i in range(0, w):
+        X[i, :] = dct(x[i, :], inverse)
+    
+    return X
+
+def dct_image(im, channels, inverse=False):
+    '''Performs 2D DCT on a PIL Image object.''' 
+    w = im.size[0]
+    h = im.size[1]
+    im_new = np.array([0]*(w * h * channels)).reshape(w, h, channels)
+
+    for c in range(0, channels):
+        im_data = np.array(im.getdata(c)).reshape(w, h)
+        channel_data = dct_2d(im_data, w, h, inverse)
+        # im_new[:][:][c] = channel_data (doesn't work)
+
+        # mother of god why
+        for x in range(0, w):
+            for y in range(0, h):
+                #if channel_data[x, y] > 255:
+                #    channel_data[x, y] = 255
+
+                #channel_data[x, y] = np.round(channel_data[x, y]).astype(int)
+                im_new[x, y, c] = channel_data[x, y]
+
+    return Image.fromarray(im_new)
+
+# Testing
+im = Image.open('test.png')
+im_freq = dct_image(im, 4)
+im_back = dct_image(im_freq, 4, True)
+im_back.show()
